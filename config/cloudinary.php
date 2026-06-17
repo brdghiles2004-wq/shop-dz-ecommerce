7c56b240fe45e23cd7bf6470d718ae7705413f7b
ghiles abdoune <ghiles21@gmail.com>
2026-06-17 11:10:54 +0100

    fix cloudinary config

---
+++ config/cloudinary.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'cloud_url'   => env('CLOUDINARY_URL'),
+    'cloud_name'  => env('CLOUDINARY_CLOUD_NAME', 'dh4kx33by'),
+    'api_key'     => env('CLOUDINARY_API_KEY', '878249653231955'),
+    'api_secret'  => env('CLOUDINARY_API_SECRET', 'pzRCoVSmw3D24EqomupdO8RLa6Y'),
+    'secure'      => true,
+    'upload_preset' => null,
+];
