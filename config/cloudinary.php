e8496fcb9b36d01a4ae5a1c2b6f881b8280ac88e
ghiles abdoune <ghiles21@gmail.com>
2026-06-18 01:05:59 +0100

    fix cloudinary config structure

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,9 +1,14 @@
 <?php
 return [
-    'cloud_url'   => env('CLOUDINARY_URL'),
-    'cloud_name'  => env('CLOUDINARY_CLOUD_NAME', 'dh4kx33by'),
-    'api_key'     => env('CLOUDINARY_API_KEY', '878249653231955'),
-    'api_secret'  => env('CLOUDINARY_API_SECRET', 'pzRCoVSmw3D24EqomupdO8RLa6Y'),
-    'secure'      => true,
+    'cloud_url' => env('CLOUDINARY_URL', 'cloudinary://878249653231955:pzRCoVSmw3D24EqomupdO8RLa6Y@dh4kx33by'),
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME', 'dh4kx33by'),
+        'api_key'    => env('CLOUDINARY_API_KEY', '878249653231955'),
+        'api_secret' => env('CLOUDINARY_API_SECRET', 'pzRCoVSmw3D24EqomupdO8RLa6Y'),
+        'secure'     => true,
+    ],
     'upload_preset' => null,
+    'fetch_image'   => false,
+    'transformation' => [],
+    'notification_url' => null,
 ];
